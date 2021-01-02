3503e0825703ad8d20593e03976caaf36e673a07
Ishimoto Shinobu <nagakamira@gmail.com>
2021-01-02 23:38:13 +0900

    prepare env
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 #![no_std]
 #![no_main]
+#![feature(global_asm)]
+#![feature(lang_items)]
 
 use core::panic::PanicInfo;
 
+#[cfg(target_arch = "x86_64")]
+global_asm!(include_str!("arch/x86_64/start.s"));
+
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn kmain() -> ! {
 	loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
 	loop {}
+}
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn eh_personality() {
+	loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() {
+    loop {}
 }
